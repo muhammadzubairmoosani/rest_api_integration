951362c85ba51b3dde65f26f75d8e00a31ba934a
Zubair Moosani <muhammadzubairmoosani@gmail.com>
2025-03-02 01:03:42 +0500

    styling modification

--- src/components/Content/TileView.jsx
+++ src/components/Content/TileView.jsx
@@ -13,12 +13,12 @@ const TileView = ({ data, onEdit, onFlag, onDelete }) => {
           onClick={() => router.push(`/details/${item.id}`)}
         >
           <h3 className="text-lg font-semibold mb-2">{item.name}</h3>
-          <p className="text-sm text-gray-600">Email: {item.email}</p>
-          <p className="text-sm text-gray-600">Username: {item.username}</p>
-          <p className="text-sm text-gray-600">Phone: {item.phone}</p>
-          <p className="text-sm text-gray-600">Website: {item.website}</p>
-          <p className="text-sm text-gray-600">Company: {item.company?.name || ''}</p>
-          <p className="text-sm text-gray-600">Adress: {`${item.address?.suite || ''}, ${item.address?.street || ''}, ${item.address?.city || ''}`}</p>
+          <p className="text-sm text-gray-600 space-y-2"><span className="font-bold text-gray-800">Email:</span> {item.email}</p>
+          <p className="text-sm text-gray-600 space-y-2"><span className="font-bold text-gray-800">Username:</span> {item.username}</p>
+          <p className="text-sm text-gray-600 space-y-2"><span className="font-bold text-gray-800">Phone:</span> {item.phone}</p>
+          <p className="text-sm text-gray-600 space-y-2"><span className="font-bold text-gray-800">Website:</span> {item.website}</p>
+          <p className="text-sm text-gray-600 space-y-2"><span className="font-bold text-gray-800">Company:</span> {item.company?.name || ''}</p>
+          <p className="text-sm text-gray-600 space-y-2"><span className="font-bold text-gray-800">Adress:</span> {`${item.address?.suite || ''}, ${item.address?.street || ''}, ${item.address?.city || ''}`}</p>
         </div>
       ))}
     </div>
